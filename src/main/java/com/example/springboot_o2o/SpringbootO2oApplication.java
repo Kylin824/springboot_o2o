66a91431df67cfb524779a9ab9c5adcb5ca8dadb
linkangying <linkangying@huya.com>
2020-05-28 14:24:37 +0800

    first commit

---
+++ src/main/java/com/example/springboot_o2o/SpringbootO2oApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_o2o;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootO2oApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootO2oApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_o2o/SpringbootO2oApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_o2o;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootO2oApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
