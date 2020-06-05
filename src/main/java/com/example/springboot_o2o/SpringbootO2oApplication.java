3652a50efdf5183b0bbcb4dae8e56e5c12b776bd
linkangying <linkangying@huya.com>
2020-06-05 21:21:33 +0800

    support redis

--- src/main/java/com/example/springboot_o2o/SpringbootO2oApplication.java
+++ src/main/java/com/example/springboot_o2o/SpringbootO2oApplication.java
@@ -3,9 +3,11 @@ package com.example.springboot_o2o;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.example.springboot_o2o/mapper")
+@EnableCaching
 public class SpringbootO2oApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/springboot_o2o/entity/Area.java
+++ src/main/java/com/example/springboot_o2o/entity/Area.java
@@ -2,6 +2,7 @@ package com.example.springboot_o2o.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -9,7 +10,10 @@ import java.util.Date;
  */
 
 @Data
-public class Area {
+public class Area implements Serializable {
+
+
+
     // id 使用Integer默认为空  如果用int默认为0
     private Integer areaId;
     // 区域名称
